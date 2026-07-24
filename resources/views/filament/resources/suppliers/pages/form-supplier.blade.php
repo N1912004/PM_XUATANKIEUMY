767bbd74e36cb3d48441ce515391eaa9c2b8fbdf
Quoc Nghi <you@example.com>
2026-07-24 13:08:39 +0700

    style(supplier): remove save draft button from supplier form action bar

--- resources/views/filament/resources/suppliers/pages/form-supplier.blade.php
+++ resources/views/filament/resources/suppliers/pages/form-supplier.blade.php
@@ -292,12 +292,6 @@
         <!-- Footer -->
         <div class="sup-bottom-bar">
             <a href="{{ \App\Filament\Resources\SupplierResource::getUrl('index') }}" class="sup-btn">{{ __('supplier.actions.cancel') }}</a>
-            <button type="button" wire:click="saveDraft" class="sup-btn">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/>
-                </svg>
-                {{ __('supplier.actions.save_draft') }}
-            </button>
             <button type="submit" class="sup-btn sup-btn-primary">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5">
                     <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"/><polyline points="17 21 17 13 7 13 7 21"/><polyline points="7 3 7 8 15 8"/>
